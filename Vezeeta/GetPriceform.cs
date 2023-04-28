6056b0da4e0c6783558f9b97143155061ce4cdd0
salllwaaa <84684264+salllwaaa@users.noreply.github.com>
2023-04-28 19:01:03 +0300

    Add Get Price Form

---
+++ Vezeeta/GetPriceform.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+namespace Vezeeta
+{
+    public partial class GetPriceform : Form
+    {
+
+        string ordb = "Data source=orcl;User Id=scott; Password=tiger;"; 
+        OracleConnection conn;
+        public GetPriceform()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "GetMedicicnePrice";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("medicineName", Medicine.Text);
+            cmd.Parameters.Add("Mprice", OracleDbType.Double, ParameterDirection.Output);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MedPrice.Text = cmd.Parameters["Mprice"].Value.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("No such medicine Found");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
